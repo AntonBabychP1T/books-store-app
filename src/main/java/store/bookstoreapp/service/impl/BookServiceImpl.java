c877d9490487f7790ecbb4ecaf6f67bf0bbbc543
Anton Babych <antonbabi13@gmail.com>
2023-12-06 09:24:39 +0200

    Add pagination and sorting to getAll and search controllers Add swagger and some description

--- src/main/java/store/bookstoreapp/service/impl/BookServiceImpl.java
+++ src/main/java/store/bookstoreapp/service/impl/BookServiceImpl.java
@@ -3,6 +3,7 @@ package store.bookstoreapp.service.impl;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import store.bookstoreapp.dto.BookDto;
@@ -37,8 +38,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream().map(bookMapper::toDto).toList();
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream().map(bookMapper::toDto).toList();
     }
 
     @Override
@@ -55,8 +56,10 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> search(BookSearchParameters searchParameters) {
+    public List<BookDto> search(BookSearchParameters searchParameters, Pageable pageable) {
         Specification<Book> bookSpecification = bookSpecificationBuilder.build(searchParameters);
-        return bookRepository.findAll(bookSpecification).stream().map(bookMapper::toDto).toList();
+        return bookRepository.findAll(bookSpecification,pageable).stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 }
