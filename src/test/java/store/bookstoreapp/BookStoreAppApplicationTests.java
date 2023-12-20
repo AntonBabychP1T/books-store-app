981550ab490333222e98710adbe7d18e67dd2e80
Anton Babych <antonbabi13@gmail.com>
2023-12-20 18:14:20 +0200

    Some changes to docker

--- src/test/java/store/bookstoreapp/BookStoreAppApplicationTests.java
+++ src/test/java/store/bookstoreapp/BookStoreAppApplicationTests.java
@@ -1,13 +1,8 @@
 package store.bookstoreapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookStoreAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
