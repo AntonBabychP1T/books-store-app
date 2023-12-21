5454d9b579a981edaaa080c9a4d6ba60a1d79163
Anton Babych <antonbabi13@gmail.com>
2023-12-21 14:36:15 +0200

    Some changes to docker and set shopping carts to existing user

--- src/test/java/store/bookstoreapp/BookStoreAppApplicationTests.java
+++ src/test/java/store/bookstoreapp/BookStoreAppApplicationTests.java
@@ -1,8 +1,12 @@
 package store.bookstoreapp;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookStoreAppApplicationTests {
 
+    @Test
+    void contextLoads(){
+    }
 }
